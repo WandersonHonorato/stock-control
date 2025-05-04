68af6cf1e99bbba620e3238d0669650eace93188
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-05-04 13:09:38 -0300

    refactor(Category): inserindo enums do centro de custo na Category

--- src/main/java/com/novaterra/stock_control/model/Category.java
+++ src/main/java/com/novaterra/stock_control/model/Category.java
@@ -1,1 +1,21 @@
+package com.novaterra.stock_control.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Category {
+
+    OFICINA(1, "Oficina"),
+    OPERACIONAL(2, "Operacional"),
+    COMUM(3, "Comum"),
+    SAUDE(4, "Saude"),
+    ADM_OPERACIONAL(6, "Adm_Operacional"),
+    RECICLADO(5, "Reciclado"),
+    ADMINISTRATIVO(7, "Administrativo");
+
+    private final int codigo;
+    private final String descricao;
+}
 
