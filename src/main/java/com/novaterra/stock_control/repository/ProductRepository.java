93855d0ce8ef33c9860163ccff69bd71c37ff256
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-05-05 20:01:23 -0300

    feat(productservice) estrutura da ServiceProduct

--- src/main/java/com/novaterra/stock_control/repository/ProductRepository.java
+++ src/main/java/com/novaterra/stock_control/repository/ProductRepository.java
@@ -1,1 +1,15 @@
+package com.novaterra.stock_control.repository;
+
+import com.novaterra.stock_control.model.Product;
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Id> {
+
+    Optional<Product> findById(Long id);
+}
 
--- src/main/java/com/novaterra/stock_control/service/ProductService.java
+++ src/main/java/com/novaterra/stock_control/service/ProductService.java
@@ -1,1 +1,32 @@
+package com.novaterra.stock_control.service;
 
+import com.novaterra.stock_control.model.Product;
+import com.novaterra.stock_control.repository.ProductRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public List<Product> listProduct() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductId(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+
+    }
+}
