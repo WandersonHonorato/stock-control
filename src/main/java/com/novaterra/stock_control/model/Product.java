be313072e965355c9e803ce6e24f351e2fb2e091
Wanderson <faturamento@novaterraambiental.srv.br>
2026-06-22 01:03:54 -0300

    refactor: ajustar anottattion de localDateTime

--- src/main/java/com/novaterra/stock_control/model/Product.java
+++ src/main/java/com/novaterra/stock_control/model/Product.java
@@ -1,18 +1,18 @@
+// Product.java
 package com.novaterra.stock_control.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 import java.time.LocalDateTime;
 
@@ -32,21 +32,20 @@ public class Product {
     @Column(name = "NOME", nullable = false, unique = true)
     private String nome;
 
-    @Size(min = 3, max = 120, message = "A descrição do produto deve conter entre {min} e {max} caracteres.")
-    @NotBlank(message = "A descrição do produto é obrigatório")
+    @Size(min = 3, max = 120, message = "A descrição deve conter entre {min} e {max} caracteres.")
+    @NotBlank(message = "A descrição do produto é obrigatória")
     @Column(name = "DESCRICAO", nullable = false)
     private String descricao;
 
-    @NotNull(message = "A Quantidade Mínima do produto é obrigatório")
-    @Column(name = "QUANTIDADE MINIMA", nullable = false)
+    @NotNull(message = "A quantidade mínima é obrigatória")
+    @Column(name = "QUANTIDADE_MINIMA", nullable = false)
     private Integer quantidadeMinima;
 
-    @NotNull(message = "A Quantidade Máxima do produto é obrigatório")
-    @Column(name = "QUANTIDADE MAXIMA", nullable = false)
+    @NotNull(message = "A quantidade máxima é obrigatória")
+    @Column(name = "QUANTIDADE_MAXIMA", nullable = false)
     private Integer quantidadeMaxima;
 
-    @Column(name = "QNT MAXIMA", nullable = false)
-    @NotBlank(message = "A data de criação do produto é obrigatório")
+    @NotNull(message = "A data de criação é obrigatória")
+    @Column(name = "CRIADO_EM", nullable = false)
     private LocalDateTime criadoEm;
 }
-
